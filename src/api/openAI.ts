3e6fa76fdc4325d573e83bc3cc25d20d64d99bf5
Phenix <phenix.teng@gmail.com>
2023-04-16 21:24:41 +0800

    feat: stream response

--- src/api/openAI.ts
+++ src/api/openAI.ts
@@ -1,29 +1,72 @@
-import { defHttp } from '/@/utils/http/axios';
-import { ErrorMessageMode } from '/#/axios';
-import { ChatParams, ChatResult, Choice_t } from './model/openAIModel';
+import { ChatParams } from './model/openAIModel';
 import axios from 'axios';
-import { ChatGPTAPI } from 'chatgpt';
+//import { ChatGPTAPI } from 'chatgpt';
 import { isArray } from 'xe-utils';
 
 enum Api {
   Chat = '/chat/completions',
 }
 
+//import { generatePayload, parseOpenAIStream } from '/@/utils/openAI';
+// #vercel-disable-blocks
+//import { fetch, ProxyAgent } from 'undici';
+// #vercel-end
+
+const apiKey = import.meta.env.OPENAI_API_KEY;
+//const httpsProxy = import.meta.env.HTTPS_PROXY;
+const baseUrl = (import.meta.env.OPENAI_API_BASE_URL || 'https://api.openai.com/v1')
+  .trim()
+  .replace(/\/$/, '');
+
+export async function chatGPTStream(
+  messages: string | ChatParams[],
+  signal: AbortSignal,
+): Promise<Response> {
+  console.log(messages);
+  const initOptions = {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${
+        apiKey ? apiKey : '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'
+      }`,
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      model: 'gpt-3.5-turbo',
+      messages: isArray(messages) ? messages : [{ role: 'user', content: messages }],
+      temperature: 0.6,
+      stream: true,
+    }),
+    signal: signal,
+  };
+
+  // #vercel-disable-blocks
+  // if (httpsProxy) {
+  //   initOptions['dispatcher'] = new ProxyAgent(httpsProxy);
+  // }
+  // #vercel-end
+
+  // @ts-ignore
+  const response = (await fetch(`https://aiassist.sytes.net/generate`, initOptions)) as Response;
+  //const response = (await fetch(`${baseUrl}${Api.Chat}`, initOptions)) as Response;
+  return response; //new Response(parseOpenAIStream(response));
+}
+
 /**
  * @description: request chat completions
  */
-function reqChatApi(params: ChatParams, mode: ErrorMessageMode = 'none') {
-  console.log(params);
-  return defHttp.post<ChatResult>(
-    {
-      url: Api.Chat,
-      params,
-    },
-    {
-      errorMessageMode: mode,
-    },
-  );
-}
+// function reqChatApi(params: ChatParams, mode: ErrorMessageMode = 'none') {
+//   console.log(params);
+//   return defHttp.post<ChatResult>(
+//     {
+//       url: Api.Chat,
+//       params,
+//     },
+//     {
+//       errorMessageMode: mode,
+//     },
+//   );
+// }
 
 /**
  * @description: chat completions api
@@ -42,7 +85,7 @@ export async function chatGPT(messages: string | ChatParams[]): Promise<ChatPara
   console.log('chatGPT', messages);
 
   const data = await axios.post(
-    'https://api.openai.com/v1/chat/completions',
+    `${baseUrl}${Api.Chat}`,
     {
       //temperature: 0.6,
       model: 'gpt-3.5-turbo',
@@ -51,7 +94,7 @@ export async function chatGPT(messages: string | ChatParams[]): Promise<ChatPara
     {
       timeout: 300000,
       headers: {
-        Authorization: 'Bearer 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',
+        Authorization: 'Bearer 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',
         'Content-Type': 'application/json',
       },
     },
